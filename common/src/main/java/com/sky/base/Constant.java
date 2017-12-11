c523a8907bbf89a46f1dea195001fc70111c4296
hebingsen <hebs1111@13322.com>
2017-12-11 14:47:14 +0800

    cros跨域

--- common/src/main/java/com/sky/base/Constant.java
+++ common/src/main/java/com/sky/base/Constant.java
@@ -1,5 +1,7 @@
 package com.sky.base;
 
+import org.springframework.http.HttpMethod;
+
 /**
  * 公共常量
  * @作者 乐此不彼
@@ -12,5 +14,19 @@ public interface Constant {
 	 * token过期时间(单位:秒)
 	 */
 	public final static long TOKEN_TIME = 3600L;
+
+	/** cors 支持的所有方法. 原则上来说, 国内复杂的浏览器环境, 开发时只支持 get 和 post 就好了, 其他忽略 */
+	public static final String[] SUPPORT_METHODS = new String[] {
+			HttpMethod.HEAD.name(),
+
+			HttpMethod.GET.name(),
+			HttpMethod.POST.name(),
+
+			HttpMethod.PUT.name(),
+			HttpMethod.DELETE.name(),
+			HttpMethod.OPTIONS.name(),
+			HttpMethod.PATCH.name(),
+			HttpMethod.TRACE.name()
+	};
 
 }
